a060a8ec2476366aeecc18ed5b078ab01a3b15cf
sam_ryong <zackinthebox@khu.ac.kr>
2024-11-28 19:11:40 +0900

    투데이 버그 개선

--- src/main/java/samryong/blogserver/WebConfig.java
+++ src/main/java/samryong/blogserver/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 URL 경로에 대해
-                .allowedOrigins("http://localhost:3000", "https://sam-ryong.github.io/info/") // 허용할 도메인 (React 개발 서버)
+                .allowedOrigins("http://localhost:3000", "https://sam-ryong.github.io/info/", "https://sam-ryong.github.io/") // 허용할 도메인 (React 개발 서버)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 인증 정보 포함 여부
